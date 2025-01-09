7ad9453ad77257f7e004ba89018858901a9e288a
TrymVei <tve@variant.no>
2025-01-09 11:35:29 +0100

    feat: add color constant

---
+++ src/utils/colors.ts
@@ -0,0 +1,14 @@
+export const colors = {
+  yellow: "#FFD02F",
+
+  background: {
+    primary: "#fafafa",
+    dark: "#2D2D2D",
+  },
+
+  text: {
+    primary: "#0A0B0B",
+    primaryOnDark: "#FAFAFA",
+    tertiary: "#5E5E5E",
+  },
+};
