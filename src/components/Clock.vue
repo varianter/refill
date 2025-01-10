c84f853285a17d4f5e9bbe5cde459c0b1f258434
TrymVei <tve@variant.no>
2025-01-10 10:19:54 +0100

    feat: clock dynamic countdown

---
+++ src/components/Clock.vue
@@ -0,0 +1,53 @@
+<template>
+  <span>{{ formattedTime }}</span>
+</template>
+
+<script>
+export default {
+  props: {
+    targetTime: {
+      type: [String, Number, Date],
+      required: true,
+    },
+    format: {
+      type: String,
+      default: "hours",
+      validator: (value) => ["days", "hours"].includes(value),
+    },
+  },
+  data() {
+    return {
+      remainingTime: this.calculateRemainingTime(),
+    };
+  },
+  computed: {
+    formattedTime() {
+      if (this.format === "days") {
+        return this.remainingTime.days;
+      } else {
+        return this.remainingTime.hours;
+      }
+    },
+  },
+  methods: {
+    calculateRemainingTime() {
+      const now = new Date();
+      const target = new Date(this.targetTime);
+      const difference = target - now;
+
+      const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((difference / (1000 * 60 * 60)) % 24);
+
+      return { days, hours };
+    },
+  },
+  mounted() {
+    this.interval = setInterval(() => {
+      this.remainingTime = this.calculateRemainingTime();
+    }, 60000);
+  },
+  beforeDestroy() {
+    clearInterval(this.interval);
+  },
+};
+</script>
