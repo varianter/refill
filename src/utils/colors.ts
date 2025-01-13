aeb831f3c3b4adfed5503cbd41c5501f980b5312
TrymVei <tve@variant.no>
2025-01-13 13:37:45 +0100

    feat: adds tilted tag component

--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -1,5 +1,6 @@
 export const colors = {
   yellow: "#FFD02F",
+  green: "#3ACA40",
 
   background: {
     primary: "#fafafa",
@@ -10,5 +11,23 @@ export const colors = {
     primary: "#0A0B0B",
     primaryOnDark: "#FAFAFA",
     tertiary: "#5E5E5E",
+  },
+};
+
+export type ColorPair = {
+  color: string;
+  textColor: string;
+};
+
+type ColorNames = "yellow" | "green";
+
+export const colorPairs: Record<ColorNames, ColorPair> = {
+  yellow: {
+    color: colors.yellow,
+    textColor: colors.text.primary,
+  },
+  green: {
+    color: colors.green,
+    textColor: colors.text.primary,
   },
 };
