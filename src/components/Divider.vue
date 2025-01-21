2105c72ef6a9f15dc66270f32de5f4bd1b6a7a14
TrymVei <tve@variant.no>
2025-01-21 10:58:08 +0100

    feat: adds Divider component

---
+++ src/components/Divider.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="divider">
+    <span class="divider__line" />
+  </div>
+</template>
+
+<style lang="css">
+.divider {
+  padding-top: 0.625rem;
+  padding-bottom: calc(var(--Padding-m) + 0.625rem);
+}
+
+.divider__line {
+  display: inline-block;
+  width: 100%;
+  height: 1px;
+  background-color: var(--Surface-yellow-subtle);
+}
+</style>
