4dd8c98e681efa3bfad2990de0c37cd097ba7605
TrymVei <tve@variant.no>
2025-01-22 11:50:35 +0100

    feat: adds card wrapper component

---
+++ src/components/CardWrapper.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+  props: {
+    maxWidth: {
+      type: String,
+      required: true,
+    },
+    height: {
+      type: String,
+      required: false,
+      default: "398px",
+    },
+  },
+};
+</script>
+<template>
+  <div class="card_wrapper">
+    <slot />
+  </div>
+</template>
+
+<style lang="css">
+.card_wrapper {
+  max-width: v-bind(maxWidth);
+  width: 100%;
+  max-height: 398px;
+  height: v-bind(height);
+  padding: var(--Padding-m);
+  background-color: var(--background-primary);
+  border-radius: 0.375rem;
+}
+</style>
