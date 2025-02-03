a56f27fe5dba8b79065b50f15b219313e12d565f
TrymVei <tve@variant.no>
2025-02-03 17:29:50 +0100

    feat: adds new card wrapper with border color

--- src/components/CardWrapper.vue
+++ src/components/CardWrapper.vue
@@ -1,4 +1,5 @@
 <script>
+import colors from "../utils/colors.ts";
 export default {
   props: {
     height: {
@@ -6,6 +7,9 @@ export default {
       required: false,
       default: "398px",
     },
+    borderColor: String,
+    required: false,
+    default: "#0A0B0B",
   },
 };
 </script>
@@ -17,17 +21,8 @@ export default {
 
 <style lang="css">
 .card_wrapper {
-  max-height: 398px;
-  height: v-bind(height);
-  box-shadow:
-    0.6px 0.7px 2.2px rgba(0, 0, 0, 0.024),
-    1.4px 1.7px 5.3px rgba(0, 0, 0, 0.032),
-    2.6px 3.1px 10px rgba(0, 0, 0, 0.038),
-    4.7px 5.6px 17.9px rgba(0, 0, 0, 0.044),
-    8.8px 10.4px 33.4px rgba(0, 0, 0, 0.053),
-    21px 25px 80px rgba(0, 0, 0, 0.07);
+  border: 1px solid v-bind(borderColor, "#0A0B0B");
   padding: var(--Padding-m);
-  background-color: var(--background-primary);
   border-radius: 0.375rem;
 }
 </style>
