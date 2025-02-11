77f1da68fa66f4b21b233b50bbdc4a9b384d16f8
TrymVei <tve@variant.no>
2025-02-11 18:50:20 +0100

    feat: adds box shadows to speaker cards

--- src/components/CardWrapper.vue
+++ src/components/CardWrapper.vue
@@ -38,6 +38,13 @@ export default {
   opacity: 1;
   height: 100%;
   box-sizing: border-box;
+  box-shadow:
+    0.6px 0.7px 2.2px rgba(0, 0, 0, 0.024),
+    1.4px 1.7px 5.3px rgba(0, 0, 0, 0.032),
+    2.6px 3.1px 10px rgba(0, 0, 0, 0.038),
+    4.7px 5.6px 17.9px rgba(0, 0, 0, 0.044),
+    8.8px 10.4px 33.4px rgba(0, 0, 0, 0.053),
+    21px 25px 80px rgba(0, 0, 0, 0.07);
 
   display: flex;
   flex-direction: column;
