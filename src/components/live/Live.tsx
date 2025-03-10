5a620ceca447f76e9a3ecc7421b6e3652c3b7d32
TrymVei <tve@variant.no>
2025-03-10 10:34:06 +0100

    feat: adds styling for live

--- src/components/live/Live.tsx
+++ src/components/live/Live.tsx
@@ -4,6 +4,7 @@ import {
   getCurrentTimeInMinutes,
   isTalkActive,
 } from "../../utils/schedule/functions";
+import style from "./Live.module.css";
 
 type LiveProps = {
   event: ScheduleEntry;
@@ -30,8 +31,16 @@ export const Live = ({ event }: LiveProps) => {
     return () => clearInterval(interval);
   }, []);
 
-  if (isLive) {
-    return <div>Live</div>;
-  }
-  return null;
+  return (
+    <div>
+      {isLive && (
+        <a className={style.live}>
+          <span className={style.live__dot}>
+            <span className={style.live__dot__inner} />
+          </span>
+          Live now!
+        </a>
+      )}
+    </div>
+  );
 };
